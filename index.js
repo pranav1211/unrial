2ba7965033583e80e22946766133a6ba6cd98455
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-05-01 19:07:50 +0530

    1/5/24

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+getdata()
 var locations;
-fetch('index.json')
-    .then(response => response.json())
-    .then(data => { console.log(data) })
-    .catch(error => console.error('Error reading JSON:', error));
 
-function geturl(data){
+function getdata() {
+    fetch('index.json')
+        .then(response => response.json())
+        .then(data => { console.log(data) })
+        .catch(error => console.error('Error reading JSON:', error));
+
+}
+function geturl(data) {
     locations = data['sitelocation']
 }
 
