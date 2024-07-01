a620b986445333b480293c10948dfb23a9cfa427
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-07-02 01:36:39 +0530

    2/7/24

--- manage.js
+++ manage.js
@@ -26,13 +26,13 @@ function changeurl(newurl) {
 http.createServer((request, response) => {
     const path = request.url;
     const substr = '/yoma'
-    if (path.includes(substr)) {
-        console.log('server called');
-        const querystring = url.split('?')[1];
-        const parameters = new URLSearchParams(querystring)
-        const data1 = parameters.get('data1')
-        changeurl(data1)
-        response.end("name changed");
+    response.end("name changed");
+    // if (path.includes(substr)) {
+    //     console.log('server called');
+    //     const querystring = url.split('?')[1];
+    //     const parameters = new URLSearchParams(querystring)
+    //     const data1 = parameters.get('data1')
+    //     changeurl(data1)        
     }
 
 }).listen(6003);
