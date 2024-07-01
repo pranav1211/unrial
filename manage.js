b6bcbb7f2d186fe43920f163cff393e5ebbe0943
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-07-01 23:52:55 +0530

    1/7/24

--- manage.js
+++ manage.js
@@ -1,31 +1,5 @@
-var getname = document.querySelector('#getname');
-var setname = document.querySelector('#setname');
-var thename = document.querySelector('#name');
-var thedata;
+const http = require('http')
 
-function getdata() {
-    fetch('index.json')
-        .then(response => response.json())
-        .then(data => { 
-            thedata = data;
-            senddata(thedata);
-        })
-        .catch(error => console.error('Error reading JSON:', error));
-}
-
-getname.addEventListener('click', () => {
-    getdata();
-});
-
-function senddata(data) {
-    thename.innerHTML = data["sitelocation"];
-}
-
-setname.addEventListener('click', () => {
-    var newname = prompt("Enter new name");
-    updateData(newname)
-});
-
-function updateData(newName) {
-    window.location = 'http://64.227.143.61:6123/qrep?data1='+ newName
-}
+http.createServer((request,response)=>{
+    response.end('Hello world')
+}).listen(6002);
