97c37fa0720de9a88fea033a2fd2343d79b9317e
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-07-02 01:31:12 +0530

    2/7/24

---
+++ buttons.js
@@ -0,0 +1,31 @@
+var getname = document.querySelector('#getname');
+var setname = document.querySelector('#setname');
+var thename = document.querySelector('#name');
+var thedata;
+
+function getdata() {
+    fetch('index.json')
+        .then(response => response.json())
+        .then(data => { 
+            thedata = data;
+            senddata(thedata);
+        })
+        .catch(error => console.error('Error reading JSON:', error));
+}
+
+getname.addEventListener('click', () => {
+    getdata();
+});
+
+function senddata(data) {
+    thename.innerHTML = data["sitelocation"];
+}
+
+setname.addEventListener('click', () => {
+    var newname = prompt("Enter new name");
+    updateData(newname)
+});
+
+function updateData(newName) {
+    window.location = 'http://64.227.143.61/yoma?data1='+ newName
+}
--- manage.js
+++ manage.js
@@ -1,11 +1,37 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
+const { json } = require('stream/consumers');
 
-http.createServer((request,response)=>{
-    var path = request.url;    
-    if(path === '/yoma') {
-        response.end("v1")
+fs.readFile('index.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
     }
-    else if(path === '/yomama'){
-        response.end("v1")
+    const jsdata = JSON.parse(data);
+});
+
+function changeurl(newurl) {
+    var newname = "https://" + newurl;
+    jsdata.sitelocation = newname;
+
+    fs.writeFile('index.json', JSON.stringify(jsdata), 'utf8', (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("Rewrite success");
+    });
+}
+
+http.createServer((request, response) => {
+    const path = request.url;
+    const substr = '/yoma'
+    if (url.includes(substr)) {
+        console.log('server called');
+        const querystring = url.split('?')[1];
+        const parameters = new URLSearchParams(querystring)
+        const data1 = parameters.get('data1')
+        changeurl(data1)
     }
+
 }).listen(6003);
